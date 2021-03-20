c8b884e7411196f7b200ae36fd35884b71b8bfb2
Andy Zhou <andy.zhou@duke.edu>
2021-03-20 12:54:59 -0500

    create methods for pushbuttons

--- landside/catkin_ws/src/gui/src/gui/controls_module.py
+++ landside/catkin_ws/src/gui/src/gui/controls_module.py
@@ -1,5 +1,10 @@
+from __future__ import print_function
+
+from std_srvs.srv import SetBool
+
 import os
 import time
+import sys
 
 import rospy
 import rospkg
@@ -42,42 +47,18 @@ class ControlsWidget(QWidget):
         self.graphics_view.setMouseTracking(True)
 
         # define buttons
-        self.pose_enable = QIcon.fromTheme()
+        self.pose_enable = QIcon.fromTheme() # fill in with icon
         self.twist_enable = QIcon.fromTheme()
-        self.pushButton = QIcon.fromTheme()
-        self.pushButton_2 = QIcon.fromTheme()
+        self.run_launch_file = QIcon.fromTheme()
+        self.controls_enable = QIcon.fromTheme()
 
-        self.play_icon = QIcon.fromTheme('media-playback-start')
-        self.pause_icon = QIcon.fromTheme('media-playback-pause')
-        self.play_button.setIcon(self.play_icon)
-        self.begin_button.setIcon(QIcon.fromTheme('media-skip-backward'))
-        self.end_button.setIcon(QIcon.fromTheme('media-skip-forward'))
-        self.slower_button.setIcon(QIcon.fromTheme('media-seek-backward'))
-        self.faster_button.setIcon(QIcon.fromTheme('media-seek-forward'))
-        self.zoom_in_button.setIcon(QIcon.fromTheme('zoom-in'))
-        self.zoom_out_button.setIcon(QIcon.fromTheme('zoom-out'))
-        self.zoom_all_button.setIcon(QIcon.fromTheme('zoom-original'))
-        self.thumbs_button.setIcon(QIcon.fromTheme('insert-image'))
-        self.record_button.setIcon(QIcon.fromTheme('media-record'))
-        self.load_button.setIcon(QIcon.fromTheme('document-open'))
-        self.save_button.setIcon(QIcon.fromTheme('document-save'))
+        self.pose_enable.clicked[bool].connect(self._handle_pose_enable_clicked)
+        self.twist_enable.clicked[bool].connect(self._handle_twist_enable_clicked)
+        self.run_launch_file.clicked[bool].connect(self._handle_run_launch_file_clicked)
+        self.controls_enable.clicked[bool].connect(self._handle_controls_enable_clicked)
 
-        self.play_button.clicked[bool].connect(self._handle_play_clicked)
-        self.thumbs_button.clicked[bool].connect(self._handle_thumbs_clicked)
-        self.zoom_in_button.clicked[bool].connect(self._handle_zoom_in_clicked)
-        self.zoom_out_button.clicked[bool].connect(self._handle_zoom_out_clicked)
-        self.zoom_all_button.clicked[bool].connect(self._handle_zoom_all_clicked)
-        self.faster_button.clicked[bool].connect(self._handle_faster_clicked)
-        self.slower_button.clicked[bool].connect(self._handle_slower_clicked)
-        self.begin_button.clicked[bool].connect(self._handle_begin_clicked)
-        self.end_button.clicked[bool].connect(self._handle_end_clicked)
-        self.record_button.clicked[bool].connect(self._handle_record_clicked)
-        self.load_button.clicked[bool].connect(self._handle_load_clicked)
-        self.save_button.clicked[bool].connect(self._handle_save_clicked)
-        self.graphics_view.mousePressEvent = self._timeline.on_mouse_down
-        self.graphics_view.mouseReleaseEvent = self._timeline.on_mouse_up
-        self.graphics_view.mouseMoveEvent = self._timeline.on_mouse_move
-        self.graphics_view.wheelEvent = self._timeline.on_mousewheel
+        # example: self.play_icon = QIcon.fromTheme('media-playback-start')
+ 
         self.closeEvent = self.handle_close
         self.keyPressEvent = self.on_key_press
         # TODO when the closeEvent is properly called by ROS_GUI implement that event instead of destroyed
@@ -143,67 +124,20 @@ class ControlsWidget(QWidget):
         # need to look at a method to determine the maximum size of the scene that will maintain a proper no scrollbar fit in the view.
         self.graphics_view.scene().setSceneRect(0, 0, self.graphics_view.width() - 2, max(self.graphics_view.height() - 2, self._timeline._timeline_frame._history_bottom))
 
-    def _handle_publish_clicked(self, checked):
-        self._timeline.set_publishing_state(checked)
+    # our methods
+    def _handle_pose_enable_clicked(self):
+        os.system('')
+    
+    def _handle_twist_enable_clicked(self):
+        os.system('')
 
-    def _handle_play_clicked(self, checked):
-        if checked:
-            self.play_button.setIcon(self.pause_icon)
-            self._timeline.navigate_play()
-        else:
-            self.play_button.setIcon(self.play_icon)
-            self._timeline.navigate_stop()
+    def _handle_run_launch_file_clicked(self):
+        os.system('roslaunch execute motion.launch') # execute a script w an arg that sends cmd to onboard
 
-    def _handle_faster_clicked(self):
-        self._timeline.navigate_fastforward()
-        self.play_button.setChecked(True)
-        self.play_button.setIcon(self.pause_icon)
+    def _handle_controls_enable_clicked(self):
+        os.system('rosservice call /enable_controls true') 
 
-    def _handle_slower_clicked(self):
-        self._timeline.navigate_rewind()
-        self.play_button.setChecked(True)
-        self.play_button.setIcon(self.pause_icon)
-
-    def _handle_begin_clicked(self):
-        self._timeline.navigate_start()
-
-    def _handle_end_clicked(self):
-        self._timeline.navigate_end()
-
-    def _handle_thumbs_clicked(self, checked):
-        self._timeline._timeline_frame.toggle_renderers()
-
-    def _handle_zoom_all_clicked(self):
-        self._timeline.reset_zoom()
-
-    def _handle_zoom_out_clicked(self):
-        self._timeline.zoom_out()
-
-    def _handle_zoom_in_clicked(self):
-        self._timeline.zoom_in()
-
-    def _handle_record_clicked(self):
-        if self._recording:
-            self._timeline.toggle_recording()
-            return
-        # TODO verify master is still running
-        filename = QFileDialog.getSaveFileName(self, self.tr('Select prefix for new Bag File'), '.', self.tr('Bag files {.bag} (*.bag)'))
-        if filename[0] != '':
-            prefix = filename[0].strip()
-
-            # Get filename to record to
-            record_filename = time.strftime('%Y-%m-%d-%H-%M-%S.bag', time.localtime(time.time()))
-            if prefix.endswith('.bag'):
-                prefix = prefix[:-len('.bag')]
-            if prefix:
-                record_filename = '%s_%s' % (prefix, record_filename)
-
-            rospy.loginfo('Recording to %s.' % record_filename)
-
-            #TODO Implement recording of topic subsets, regex limiting and by number of messages per topic
-            self.load_button.setEnabled(False)
-            self._recording = True
-            self._timeline.record_bag(record_filename)
+    # preexisting methods that aren't necessary 
 
     def _handle_load_clicked(self):
         filename = QFileDialog.getOpenFileName(self, self.tr('Load from File'), '.', self.tr('Bag files {.bag} (*.bag)'))
@@ -230,43 +164,6 @@ class ControlsWidget(QWidget):
         if filename[0] != '':
             self._timeline.copy_region_to_bag(filename[0])
 
-    def _update_status_bar(self):
-        if self._timeline._timeline_frame.playhead is None or self._timeline._timeline_frame.start_stamp is None:
-            return
-        # TODO Figure out why this function is causing a "RuntimeError: wrapped C/C++ object of %S has been deleted" on close if the playhead is moving
-        try:
-            # Background Process Status
-            self.progress_bar.setValue(self._timeline.background_progress)
-
-            # Raw timestamp
-            self.stamp_label.setText('%.3fs' % self._timeline._timeline_frame.playhead.to_sec())
-
-            # Human-readable time
-            self.date_label.setText(bag_helper.stamp_to_str(self._timeline._timeline_frame.playhead))
-
-            # Elapsed time (in seconds)
-            self.seconds_label.setText('%.3fs' % (self._timeline._timeline_frame.playhead - self._timeline._timeline_frame.start_stamp).to_sec())
-
-            # Play speed
-            spd = self._timeline.play_speed
-            if spd != 0.0:
-                if spd > 1.0:
-                    spd_str = '>> %.0fx' % spd
-                elif spd == 1.0:
-                    spd_str = '>'
-                elif spd > 0.0:
-                    spd_str = '> 1/%.0fx' % (1.0 / spd)
-                elif spd > -1.0:
-                    spd_str = '< 1/%.0fx' % (1.0 / -spd)
-                elif spd == 1.0:
-                    spd_str = '<'
-                else:
-                    spd_str = '<< %.0fx' % -spd
-                self.playspeed_label.setText(spd_str)
-            else:
-                self.playspeed_label.setText('')
-        except:
-            return
     # Shutdown all members
 
     def shutdown_all(self):
